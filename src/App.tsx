5d9cce7e401658f5adbd1a433c4bb7a453d3b7dd
yogeshbpatil <patilyogeshkumarb@gmail.com>
2025-10-17 23:11:30 +0530

    fix: removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // src/App.tsx
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import UserForm from './pages/Form/UserForm';
